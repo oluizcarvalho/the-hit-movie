27f9d733c0a599a926da6f5ae90dd7069070681a
Luiz Carvalho <luizandre.ita@gmail.com>
2023-02-26 18:20:44 -0300

    (feat): adicionando pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [CoreModule, AppRoutingModule],
+	imports: [
+		CoreModule,
+		AppRoutingModule,
+		ServiceWorkerModule.register('ngsw-worker.js', {
+			enabled: environment.production,
+			registrationStrategy: 'registerWhenStable:30000',
+		}),
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
